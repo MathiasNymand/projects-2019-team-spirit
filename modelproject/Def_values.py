e9fc567524fb807e686be2438d378c68531da26d
MathiasNymand <47112200+MathiasNymand@users.noreply.github.com>
2019-05-07 13:59:07 +0200

    heee

---
+++ modelproject/Def_values.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  7 13:42:42 2019
+
+@author: Mathias
+"""
+
+
+s = 0.25
+g_b = 0.015
+n = 0.01
+alpha = 0.25
+delta = 0.035
+theta = 0.05
+g_a = 0.03
---
+++ modelproject/Def_variables.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  7 13:03:21 2019
+
+@author: Mathias
+"""
+
+import sympy as sm
+
+K = sm.symbols('K')
+L = sm.symbols('L')
+Y = sm.symbols('Y')
+B = sm.symbols('B')
+Q = sm.symbols('Q')
+R = sm.symbols('R')
+E = sm.symbols('E')
+z = sm.symbols('z')
+Omega = sm.symbols('Omega')
+alpha = sm.symbols('alpha')
+delta = sm.symbols('delta')
+s = sm.symbols('s')
+g_a = sm.symbols('g_a')
+n = sm.symbols('n')
+theta = sm.symbols('theta')
+A = sm.symbols('A')
+epsilon = sm.symbols('epsilon')
+B_t = sm.symbols('B_t')
+K_t = sm.symbols('K_t')
+L_t = sm.symbols('L_t')
+Omega_t = sm.symbols('Omega_t')
+k_t = sm.symbols('k_t')
+k = sm.symbols('k')
+y = sm.symbols('y')
+L_0 = sm.symbols('L_0')
+t = sm.symbols('t')
+g_b = sm.symbols('g_b')
+B_0 = sm.symbols('B_0')
+Omega_0 = sm.symbols('Omega_0')
+
